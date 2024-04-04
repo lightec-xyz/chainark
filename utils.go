d019db9740f2f1c935fdc91a57146c321b3e73b5
Weiji Guo <weiji.g@gmail.com>
2024-04-04 22:04:12 +0800

    added utility function

--- utils.go
+++ utils.go
@@ -58,3 +58,54 @@ func AssertValsVSWtnsElements[FR emulated.FieldParams](
 		api.AssertIsEqual(vars[i], composed)
 	}
 }
+
+func IsIDEqualToWitness[FR emulated.FieldParams](
+	api frontend.API, id LinkageID, witnessValues []emulated.Element[FR], nbMaxBitsPerVar ...uint,
+) frontend.Variable {
+	return TestValsVSWtnsElements[FR](api, id.Vals, witnessValues, nbMaxBitsPerVar...)
+}
+
+func TestValsVSWtnsElements[FR emulated.FieldParams](
+	api frontend.API, vars []frontend.Variable, witnessValues []emulated.Element[FR], nbMaxBitsPerVar ...uint,
+) frontend.Variable {
+	api.AssertIsEqual(len(witnessValues), len(vars))
+
+	var fr FR
+	var maxBits int
+
+	bitsPerLimb := int(FR.BitsPerLimb(fr))
+	if len(nbMaxBitsPerVar) == 0 {
+		maxBits = int(fr.NbLimbs() * fr.BitsPerLimb())
+	} else {
+		maxBits = int(nbMaxBitsPerVar[0])
+	}
+
+	nbEffectiveLimbs := int((maxBits + bitsPerLimb - 1) / bitsPerLimb)
+
+	for i := 0; i < len(witnessValues); i++ {
+		for j := nbEffectiveLimbs; j < int(fr.NbLimbs()); j++ {
+			api.AssertIsEqual(witnessValues[i].Limbs[j], 0)
+		}
+	}
+
+	constFactor := big.NewInt(1)
+	for i := 0; i < int(bitsPerLimb); i++ {
+		constFactor = constFactor.Mul(constFactor, big.NewInt(2))
+	}
+
+	sum := frontend.Variable(1)
+	for i := 0; i < len(vars); i++ {
+		eleLimbs := witnessValues[i].Limbs
+		composed := eleLimbs[nbEffectiveLimbs-1]
+		for j := nbEffectiveLimbs - 2; j >= 0; j-- {
+			v := api.Mul(composed, constFactor)
+			composed = api.Add(v, eleLimbs[j])
+		}
+
+		diff := api.Sub(vars[i], composed)
+		t := api.IsZero(diff)
+		sum = api.And(sum, t)
+	}
+
+	return sum
+}
---
+++ utils_test.go
@@ -0,0 +1,78 @@
+package chainark
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/consensys/gnark-crypto/ecc"
+	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/algebra/emulated/sw_bn254"
+	"github.com/consensys/gnark/std/math/emulated"
+	"github.com/consensys/gnark/test"
+)
+
+type idTestCircuit struct {
+	Eles     [2]emulated.Element[sw_bn254.ScalarField]
+	Id       LinkageID
+	Expected frontend.Variable
+}
+
+func (c *idTestCircuit) Define(api frontend.API) error {
+	t := IsIDEqualToWitness[sw_bn254.ScalarField](api, c.Id, c.Eles[:], 128)
+	api.AssertIsEqual(t, c.Expected)
+
+	return nil
+}
+
+func TestIdVSWitness(t *testing.T) {
+	limbs1 := [4]uint64{0x012345, 0x6789ab, 0, 0}
+	limbs2 := [4]uint64{0xaa, 0xbb, 0, 0}
+	ele1 := emulated.Element[sw_bn254.ScalarField]{
+		Limbs: []frontend.Variable{
+			frontend.Variable(limbs1[0]),
+			frontend.Variable(limbs1[1]),
+			frontend.Variable(limbs1[2]),
+			frontend.Variable(limbs1[3]),
+		},
+	}
+	ele2 := emulated.Element[sw_bn254.ScalarField]{
+		Limbs: []frontend.Variable{
+			frontend.Variable(limbs2[0]),
+			frontend.Variable(limbs2[1]),
+			frontend.Variable(limbs2[2]),
+			frontend.Variable(limbs2[3]),
+		},
+	}
+	eles := [2]emulated.Element[sw_bn254.ScalarField]{ele1, ele2}
+
+	bi1, ok := big.NewInt(0).SetString("6789ab0000000000012345", 16)
+	if !ok {
+		panic("bi1")
+	}
+	bi2, ok := big.NewInt(0).SetString("bb00000000000000aa", 16)
+	if !ok {
+		panic("bi2")
+	}
+	id := LinkageID{Vals: []frontend.Variable{frontend.Variable(bi1), frontend.Variable(bi2)}, BitsPerVar: 128}
+
+	circuit := &idTestCircuit{
+		Id: PlaceholderLinkageID(2, 128),
+	}
+	assignment := &idTestCircuit{
+		Eles:     eles,
+		Id:       id,
+		Expected: 1,
+	}
+	err := test.IsSolved(circuit, assignment, ecc.BN254.ScalarField())
+	assert := test.NewAssert(t)
+	assert.NoError(err)
+
+	eles[0].Limbs[1] = frontend.Variable(100)
+	assignment2 := &idTestCircuit{
+		Eles:     eles,
+		Id:       id,
+		Expected: 0,
+	}
+	err = test.IsSolved(circuit, assignment2, ecc.BN254.ScalarField())
+	assert.NoError(err)
+}
