abf13a1fd1d9e3f014682fdf982c953257e89bba
Albert·Gou <ggq89@qq.com>
2025-02-24 18:52:51 +0800

    fix codes from common

--- types.go
+++ types.go
@@ -1,11 +1,13 @@
 package chainark
 
 import (
+	"math"
 	"slices"
 
 	common_utils "github.com/lightec-xyz/common/utils"
 
 	"github.com/consensys/gnark/frontend"
+	"github.com/consensys/gnark/std/math/bits"
 	"github.com/consensys/gnark/std/math/uints"
 )
 
@@ -13,46 +15,39 @@ type LinkageID struct {
 	Vals       []frontend.Variable
 	BitsPerVar int
 }
-type LinkageIDBytes []byte
 
-func NewLinkageID(v []frontend.Variable, b int) LinkageID {
-	return LinkageID{
-		Vals:       v,
-		BitsPerVar: b,
-	}
-}
 func PlaceholderLinkageID(nbEles, bitsPerVar int) LinkageID {
 	return LinkageID{
 		Vals:       make([]frontend.Variable, nbEles),
 		BitsPerVar: bitsPerVar,
 	}
 }
+
 func (id LinkageID) AssertIsEqual(api frontend.API, other LinkageID) {
-	if id.BitsPerVar != other.BitsPerVar {
-		panic("BitsPerVar not equal")
-	}
-	if len(id.Vals) != len(other.Vals) {
-		panic("Vals count not eual")
-	}
+	api.AssertIsEqual(id.BitsPerVar, other.BitsPerVar)
+	api.AssertIsEqual(len(id.Vals), len(other.Vals))
+
 	for i := 0; i < len(id.Vals); i++ {
 		api.AssertIsEqual(id.Vals[i], other.Vals[i])
 	}
 }
+
 func (id LinkageID) IsEqual(api frontend.API, other LinkageID) frontend.Variable {
 	if id.BitsPerVar != other.BitsPerVar {
 		panic("BitsPerVar not equal")
 	}
 	return common_utils.AreVarsEquals(api, id.Vals, other.Vals)
 }
-func (id LinkageID) ToBytes(api frontend.API) ([]uints.U8, error) {
-	vals := make([]frontend.Variable, len(id.Vals))
-	copy(vals, id.Vals)
-	slices.Reverse[[]frontend.Variable](vals)
 
-	return common_utils.ValsToU8s(api, vals, id.BitsPerVar)
+type LinkageIDBytes []byte
+
+func LinkageIDFromBytes(data LinkageIDBytes, bitsPerVar int) LinkageID {
+	return LinkageID{
+		Vals:       common_utils.ValsFromBytes(data, bitsPerVar),
+		BitsPerVar: bitsPerVar,
+	}
 }
 
-// little-endian here
 func LinkageIDFromU8s(api frontend.API, data []uints.U8, bitsPerVar int) LinkageID {
 	bits := make([]frontend.Variable, len(data)*8)
 	for i := 0; i < len(data); i++ {
@@ -60,16 +55,79 @@ func LinkageIDFromU8s(api frontend.API, data []uints.U8, bitsPerVar int) Linkage
 		copy(bits[i*8:(i+1)*8], bs)
 	}
 
-	vals := common_utils.BitsToVars(api, bits, bitsPerVar)
+	vals := bitsToVars(api, bits, bitsPerVar)
 
 	return LinkageID{
 		Vals:       vals,
 		BitsPerVar: bitsPerVar,
 	}
 }
-func LinkageIDFromBytes(data LinkageIDBytes, bitsPerVar int) LinkageID {
-	return LinkageID{
-		Vals:       common_utils.ValsFromBytes(data, bitsPerVar),
-		BitsPerVar: bitsPerVar,
+
+func bitsToVars(api frontend.API, bits []frontend.Variable, bitsPerVar int) []frontend.Variable {
+
+	vals := make([]frontend.Variable, 0)
+	for i := 0; i < len(bits); i += bitsPerVar {
+		val := api.FromBinary(bits[i : i+bitsPerVar]...)
+		vals = append(vals, val)
 	}
+
+	slices.Reverse[[]frontend.Variable](vals)
+	return vals
+}
+
+func (id LinkageID) ToBytes(api frontend.API) ([]uints.U8, error) {
+	vals := make([]frontend.Variable, len(id.Vals))
+	copy(vals, id.Vals)
+	slices.Reverse[[]frontend.Variable](vals)
+
+	return ValsToU8s(api, vals, id.BitsPerVar)
+}
+
+func ValsToU8s(api frontend.API, vals []frontend.Variable, bitsPerVar int) ([]uints.U8, error) {
+	uapi, err := uints.New[uints.U32](api)
+	if err != nil {
+		return nil, err
+	}
+
+	bytesPerVar := bitsPerVar / 8
+	ret := make([]uints.U8, bytesPerVar*len(vals))
+	for i := 0; i < len(vals); i++ {
+		bytes := byteArrayValueOf(api, uapi, vals[i], bytesPerVar)
+		begin := i * bytesPerVar
+		end := begin + bytesPerVar
+		copy(ret[begin:end], bytes)
+	}
+
+	return ret, nil
+}
+
+// Convert any varialbe to bits first then to U8 array
+// Note that if expectedLen is shorter than actual value, the converted value is *not*
+// equal to the original value!
+// TODO optimization
+func byteArrayValueOf[T uints.U32 | uints.U64](api frontend.API, bf *uints.BinaryField[T], a frontend.Variable, expectedLen ...int) []uints.U8 {
+	var opt bits.BaseConversionOption
+	var bs []frontend.Variable
+	if len(expectedLen) == 1 {
+		opt = bits.WithNbDigits(expectedLen[0] * 8)
+		bs = bits.ToBinary(api, a, opt)
+	} else {
+		bs = bits.ToBinary(api, a)
+	}
+
+	lenBits := len(bs)
+	lenBytes := int(math.Ceil(float64(lenBits) / 8.0))
+
+	ret := make([]uints.U8, lenBytes)
+	for i := 0; i < lenBytes; i++ {
+		b := bs[i*8]
+		for j := 1; j < 8 && i*8+j < lenBits; j++ {
+			v := bs[i*8+j]
+			v = api.Mul(v, 1<<j)
+			b = api.Add(b, v)
+		}
+		ret[i] = bf.ByteValueOf(b)
+	}
+
+	return ret
 }
--- types_test.go
+++ types_test.go
@@ -35,6 +35,7 @@ func (c *IDCircuit) Define(api frontend.API) error {
 
 func TestLinkageID(t *testing.T) {
 	assert := test.NewAssert(t)
+
 	idHex := "18c4c25dc847bbc76fd3ca67fc4c2028dee5263fddcf01de3faddc20f0462d8f"
 	idBytes, err := hex.DecodeString(idHex)
 	assert.NoError(err)
@@ -49,5 +50,83 @@ func TestLinkageID(t *testing.T) {
 	}
 
 	err = test.IsSolved(&circuit, &witness, ecc.BN254.ScalarField())
+	assert.NoError(err)
+}
+
+type byteArrayValueOfCircuitWithSpecifiedLen struct {
+	In       frontend.Variable
+	Expected []uints.U8
+}
+
+func (c *byteArrayValueOfCircuitWithSpecifiedLen) Define(api frontend.API) error {
+	uapi, err := uints.New[uints.U32](api)
+	if err != nil {
+		return err
+	}
+
+	res := byteArrayValueOf(api, uapi, c.In, 3)
+	api.AssertIsEqual(len(res), len(c.Expected))
+	for i := 0; i < len(res); i++ {
+		uapi.ByteAssertEq(res[i], c.Expected[i])
+	}
+
+	return nil
+}
+
+func TestByteArrayValueOfWithSpecifiedLen(t *testing.T) {
+	assert := test.NewAssert(t)
+	a, b, c := 13, 17, 19
+	p := a + (b << 8) + (c << 16)
+	expected := uints.NewU8Array([]uint8{uint8(a), uint8(b), uint8(c)})
+
+	circuit := &byteArrayValueOfCircuitWithSpecifiedLen{
+		Expected: expected,
+	}
+	assignment := &byteArrayValueOfCircuitWithSpecifiedLen{
+		In:       frontend.Variable(p),
+		Expected: expected,
+	}
+
+	err := test.IsSolved(circuit, assignment, ecc.BN254.ScalarField())
+	assert.NoError(err)
+}
+
+type byteArrayValueOfCircuitWithoutSpecifiedLen struct {
+	In       frontend.Variable
+	Expected []uints.U8
+}
+
+func (c *byteArrayValueOfCircuitWithoutSpecifiedLen) Define(api frontend.API) error {
+	uapi, err := uints.New[uints.U32](api)
+	if err != nil {
+		return err
+	}
+
+	res := byteArrayValueOf(api, uapi, c.In)
+	for i := 0; i < len(c.Expected); i++ {
+		uapi.ByteAssertEq(res[i], c.Expected[i])
+	}
+	for i := len(c.Expected); i < len(res); i++ {
+		uapi.ByteAssertEq(res[i], uints.NewU8(0))
+	}
+
+	return nil
+}
+
+func TestByteArrayValueOfWithoutSpecifiedLen(t *testing.T) {
+	assert := test.NewAssert(t)
+	a, b, c := 13, 17, 19
+	p := a + (b << 8) + (c << 16)
+	expected := uints.NewU8Array([]uint8{uint8(a), uint8(b), uint8(c)})
+
+	circuit := &byteArrayValueOfCircuitWithoutSpecifiedLen{
+		Expected: expected,
+	}
+	assignment := &byteArrayValueOfCircuitWithoutSpecifiedLen{
+		In:       frontend.Variable(p),
+		Expected: expected,
+	}
+
+	err := test.IsSolved(circuit, assignment, ecc.BN254.ScalarField())
 	assert.NoError(err)
 }
